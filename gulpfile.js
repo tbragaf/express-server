ee4b4e79d01e2bcaf489bd608475d96934b55a17
tbragaf <braga.tiago.93@gmail.com>
2016-09-13 17:19:40 +0100

    Allow production and development mode for gulp tasks and allow .env environment definition

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
+let gulp = require('gulp');
+let sourcemaps = require('gulp-sourcemaps');
+let babel = require('gulp-babel');
+let concat = require('gulp-concat');
+let clean = require('gulp-clean');
+let gutil = require('gulp-util');
 
-gulp.task('default', () => {
+gulp.task('clean', () => {
+  return gulp.src(['dist', 'dev'])
+    .pipe(clean({ force: true }));
+});
+
+gulp.task('transpile:dist', () => {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
@@ -11,3 +18,21 @@ gulp.task('default', () => {
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('transpile:dev', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dev'));
+});
+
+gulp.task('dist', ['transpile:dist']);
+
+gulp.task('dev', ['transpile:dev']);
+
+gulp.task('watch', () => {
+  return gulp.watch('src/**/*.js', ['transpile:dist']);
+});
+
+gulp.task('default', [
+  gutil.env.production ? 'dist' : 'dev'
+]);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import  express from 'express';
+import dotenv from 'dotenv';
+import express from 'express';
 import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 
-let app = express();
+import { Server } from './server.js';
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
+dotenv.config();
 
-// Start the server
-app.set('port', process.env.PORT || 3000);
+let server = new Server();
+console.log(server.app);
 
-let server = app.listen(app.get('port'), () => {
-  console.log('Server listening on port ' + server.address().port);
-});
+// let app = express();
+
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+
+// // Start the server
+// app.set('port', process.env.PORT || 3000);
+
+// let server = app.listen(app.get('port'), () => {
+//   console.log('Server listening on port ' + server.address().port);
+// });
